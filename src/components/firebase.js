9387df96e8f583fe93c3fab8a4d73b32cd4c2128
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-05-08 08:31:51 -0600

    .env file

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGzjYlHMClyCmzydZldnUsgLyHrBm2zFw",
-  authDomain: "linkedin-clone-yt-703db.firebaseapp.com",
-  projectId: "linkedin-clone-yt-703db",
-  storageBucket: "linkedin-clone-yt-703db.appspot.com",
-  messagingSenderId: "323889424653",
-  appId: "1:323889424653:web:c5dfcf86eedf244b51e844"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROTECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
